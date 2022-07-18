50dd51fd7d646e0eda1c71323ac9d99587ea33b5
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-18 14:30:49 -0300

    add element nav to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,38 +1,42 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './header.css';
+import icon from '../../images/icon.png';
 
 function Header() {
   const activeClassName = 'active';
 
   return (
     <header>
-      <ul>
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? activeClassName : undefined)}
-          >
-            Rockets
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="missions"
-            className={({ isActive }) => (isActive ? activeClassName : undefined)}
-          >
-            Missions
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="myprofile"
-            className={({ isActive }) => (isActive ? activeClassName : undefined)}
-          >
-            My Profile
-          </NavLink>
-        </li>
-      </ul>
+      <img className="logo" alt="icon of the logo page" src={icon} />
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? activeClassName : undefined)}
+            >
+              Rockets
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="missions"
+              className={({ isActive }) => (isActive ? activeClassName : undefined)}
+            >
+              Missions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="myprofile"
+              className={({ isActive }) => (isActive ? activeClassName : undefined)}
+            >
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
