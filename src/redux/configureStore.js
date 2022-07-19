f111ee437401999a345d18c807a6801f2f683f37
unknown <luispomare92@gmail.com>
2022-07-19 00:42:00 -0500

    Add mission reducer to store root reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketsSlice from './rocket/rocket';
+import missions from './mission/missions';
 
 const rootReducer = {
   rockets: rocketsSlice.reducer,
+  missions,
 };
 
 const store = configureStore({ reducer: rootReducer });
