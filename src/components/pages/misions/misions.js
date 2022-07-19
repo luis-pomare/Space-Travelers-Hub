a5c45df11fcba94c490e7edd0b2d93288c48d777
unknown <luispomare92@gmail.com>
2022-07-19 08:20:31 -0500

    Render basic missions state with useSelector and useState

--- src/components/pages/misions/misions.js
+++ src/components/pages/misions/misions.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Missions() {
+  const [missions] = useState(useSelector((state) => state.missions));
+
   return (
     <main>
       <table>
@@ -9,7 +12,10 @@ export default function Missions() {
             <th>Mission</th>
             <th>Description</th>
             <th>Status</th>
-            <th> </th>
+            <th>
+              {' '}
+              {missions[0].name}
+            </th>
           </tr>
         </thead>
         <tbody>{}</tbody>
