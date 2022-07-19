f61ae17b04d1fbaf643aea22384a509376cd06f7
unknown <luispomare92@gmail.com>
2022-07-18 23:28:14 -0500

    Utilice use efect to fetch data from the api

--- src/components/pages/misions/misions.js
+++ src/components/pages/misions/misions.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Missions() {
+  const state = useSelector((state) => state.rockets);
+  let id;
+  useEffect(() => {
+    const call = async () => {
+      const response = await fetch('https://api.spacexdata.com/v3/missions');
+      const missions = await response.json();
+      id = missions[0].id;
+      console.log(state);
+    };
+    call();
+  }, []);
+
   return (
-    <h1>Hello 123!</h1>
+    <main>
+      <table>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> {id}</th>
+          </tr>
+        </thead>
+        <tbody>{}</tbody>
+      </table>
+    </main>
   );
 }
