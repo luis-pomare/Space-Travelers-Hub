c885dd93f10eece0466d7da02956f65383d9c0e1
unknown <luispomare92@gmail.com>
2022-07-18 20:33:48 -0500

    Eliminate commented code and console log, Add provider

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,8 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import rocketsSlice from './rocket/rocket';
 
 const rootReducer = {
-  // books: bookReducer.reducer,
-  // categories: categoriesReducer.reducer,
   rockets: rocketsSlice.reducer,
 };
 
