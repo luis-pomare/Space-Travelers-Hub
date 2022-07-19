0ff42990eaeb44d528ea98aa24d934086151aa50
unknown <luispomare92@gmail.com>
2022-07-19 00:37:36 -0500

    Create missions reducer}

--- src/redux/mission/missions.js
+++ src/redux/mission/missions.js
@@ -1,1 +1,16 @@
-// createSlice
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+const missionSlice = createSlice({
+  name: 'missions',
+  initialState,
+  reducers: {
+    getMissions(action) {
+      action.payload;
+    },
+  },
+});
+
+export const { getMissions } = missionSlice.actions;
+export default missionSlice.reducer;
