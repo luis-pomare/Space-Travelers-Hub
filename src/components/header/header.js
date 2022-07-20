4d0b0bf448b3954a31cbad26f73d4b439c4f63a6
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-20 19:29:50 -0300

    add styling for header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,37 +7,47 @@ function Header() {
   const activeClassName = 'active';
 
   return (
-    <header>
-      <img className="logo" alt="icon of the logo page" src={icon} />
-      <nav>
-        <ul>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? activeClassName : undefined)}
-            >
-              Rockets
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="missions"
-              className={({ isActive }) => (isActive ? activeClassName : undefined)}
-            >
-              Missions
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="myprofile"
-              className={({ isActive }) => (isActive ? activeClassName : undefined)}
-            >
-              My Profile
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header>
+        <div className="container-logo">
+          <img className="logo" alt="icon of the logo page" src={icon} />
+          <h1> Space Travelers&apos; Hub</h1>
+        </div>
+        <nav>
+          <ul className="links-container">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? activeClassName : undefined)}
+              >
+                Rockets
+              </NavLink>
+            </li>
+            <span className="status-separator" />
+            <li>
+              <NavLink
+                to="missions"
+                className={({ isActive }) => (isActive ? activeClassName : undefined)}
+              >
+                Missions
+              </NavLink>
+            </li>
+            <span className="status-separator" />
+            <li>
+              <NavLink
+                to="myprofile"
+                className={({ isActive }) => (isActive ? activeClassName : undefined)}
+              >
+                My Profile
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="container-line">
+        <hr className="header-line" />
+      </div>
+    </>
   );
 }
 
