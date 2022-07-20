b20e89912ca54efcdfc48bdf5c3a7add9012c883
unknown <luispomare92@gmail.com>
2022-07-20 16:37:43 -0500

    Implement my missions profile filter

--- src/components/pages/myProfile/myMissions.js
+++ src/components/pages/myProfile/myMissions.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function MyMissions() {
+  const missions = useSelector((state) => state.missions);
+  const selectedMissions = missions.filter((item) => item.joined);
+
   return (
     <section>
-      <h2 id="myMissionsTitle">My Rockets</h2>
-      <span id="myMissionsContainer" />
+      <h2 id="myMissionsTitle">My Missions</h2>
+      <span id="myMissionsContainer">
+        {selectedMissions.length > 0 ? (
+          selectedMissions.map((item) => (
+            <p key={item.id}>{item.mission_name}</p>
+          ))
+        ) : (
+          <p>Not missions joined yet</p>
+        )}
+      </span>
     </section>
   );
 }
