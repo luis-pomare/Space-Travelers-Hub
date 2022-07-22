3a99574062f1e60c7a6df3b2c293b20edc69b517
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-21 21:46:10 -0300

    add test for header component

---
+++ src/components/header/header.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+
+import Header from './header';
+
+test('header displayed', () => {
+  render(<BrowserRouter>
+    <Header />
+  </BrowserRouter>);
+  expect(document.querySelector('header')).toBeInTheDocument();
+});
